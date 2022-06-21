8a16987e3def6ca9810ba40efb26893046224660
Nicolas <github@liliwol.fr>
2022-06-21 21:52:04 +0200

    Form Validator

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsComponent } from './components/forms/forms.component';
 import { HomeComponent } from './components/home/home.component';
 import { ObservableComponent } from './components/observable/observable.component';
 import { PipesComponent } from './components/pipes/pipes.component';
@@ -14,6 +15,11 @@ const routes : Routes = [
   {
     path: 'todoList',
     component: TodoListComponent
+  },
+
+  {
+    path: 'forms',
+    component: FormsComponent
   },
 
   {
--- src/app/password-validator.ts
+++ src/app/password-validator.ts
@@ -1,27 +1,17 @@
-import { FormControl, ValidationErrors, Validators } from "@angular/forms";
+import { FormControl } from "@angular/forms";
 
 
-export class PasswordValidator {
-
-	static containsKeyword ( control: FormControl ) : any
+export function containsKeyword ( control: FormControl ) : { [key: string]: boolean } | null {
 	{
 		console.log("Valeur du controle " + control.value );
 
 		console.log("Valide ? " +control.value.indexOf('angular') );
 
 		// Test de la valeur du control
-		if (control.value.indexOf('angular') != -1 ){
-			return { containsKeyword:
-				{
-					valid: true
-				}
-			};
+		if (control.value.indexOf('angular') == -1 ){
+			return { 'containsKeyword': true };
+		}else{
+			return null;
 		}
-
-		return { containsKeyword:
-			{
-				valid: false
-			}
-		};;
 	}
 }
